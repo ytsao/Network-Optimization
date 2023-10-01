397a07d6f6a39a6cc14fe7272fa2c56473a15914
YiNungTsao <t104370040@ntut.org.tw>
2023-10-01 17:53:45 +0200

    finish prim algorithm

--- prim.cpp
+++ prim.cpp
@@ -2,32 +2,34 @@
 
 int main(){
     int num_nodes = 5;
-    std::vector<int> adjacency_nodes(num_nodes);
+    std::vector<int> adjacency_nodes(num_nodes, -1);
     int adjacency_matrix[num_nodes][num_nodes] = {{0, 5, 2, 0, 0},
                                                   {5, 0, 0, 2, 4},
                                                   {2, 0, 0, 0, 10},
                                                   {0, 2, 0, 0, 1},
                                                   {0, 4, 10, 1, 0}};
 
-    int start_node = 0;
-    adjacency_nodes[start_node] = 1;
     int count = 0;
     int results = 0;
+    adjacency_nodes[0] = -1;
     while (count < num_nodes) {
         int min_value = INT_MAX;
+        int start_node = 0;
         int next_node = 0;
         for (int i = 0; i < num_nodes; ++i){
-            for (int j = 0; j < num_nodes; ++j){
+            for (int j = i + 1; j < num_nodes; ++j){
                 if (adjacency_matrix[i][j] > 0 
                 && adjacency_matrix[i][j] < min_value 
-                && adjacency_nodes[i] == 1
-                && adjacency_nodes[j] == 0){
+                && adjacency_nodes[i] != -1
+                && adjacency_nodes[j] == -1){
                     min_value = adjacency_matrix[i][j];
+                    start_node = i;
                     next_node = j;
                 }
             }
         }
-        adjacency_nodes[next_node] = 1;
+        adjacency_nodes[next_node] = start_node;
+        std::cout << "start node:" << start_node << "\n";
         std::cout << "next node:" << next_node << "\n";
         if (min_value != INT_MAX) results += min_value;
         count++;
