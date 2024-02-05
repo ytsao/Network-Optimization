c81bbcba32099791fa095b65c21c032c2c577ef2
ytsao <yi-nung.tsao@uni.lu>
2024-02-05 11:25:19 +0800

    update

--- Bellman_ford.cpp
+++ Bellman_ford.cpp
@@ -7,31 +7,31 @@
 
 
 /*
-* cost: the cost for node i 
+* cost: the cost for node i
 */
-int main(int argc, char **argv){
-    
+int main(int argc, char** argv) {
+
     int num_nodes = 7;
 
-    int adjacency_matrix[num_nodes][num_nodes] = {{0, 6, 5, 5, 0, 0, 0},
+    int adjacency_matrix[num_nodes][num_nodes] = { {0, 6, 5, 5, 0, 0, 0},
                                                   {0, 0, 0, 0, -1, 0, 0},
                                                   {0, -2, 0, 0, 1, 0, 0},
                                                   {0, 0, -2, 0, 0, -1, 0},
                                                   {0, 0, 0, 0, 0, 0, 3},
                                                   {0, 0, 0, 0, 0, 0, 3},
-                                                  {0, 0, 0, 0, 0, 0, 0}};
-    
+                                                  {0, 0, 0, 0, 0, 0, 0} };
+
     int cost[num_nodes];
     cost[0] = 0;
-    for(int i = 1; i < num_nodes; ++i){
+    for (int i = 1; i < num_nodes; ++i) {
         cost[i] = INT_MAX;
     }
 
-    for (int k = 0; k < num_nodes - 1; ++k){
-        for (int i = 0; i < num_nodes; ++i){
-            for(int j = 0; j < num_nodes; ++j){
-                if (adjacency_matrix[i][j] != 0){
-                    if (cost[i] + adjacency_matrix[i][j] < cost[j]){
+    for (int k = 0; k < num_nodes - 1; ++k) {
+        for (int i = 0; i < num_nodes; ++i) {
+            for (int j = 0; j < num_nodes; ++j) {
+                if (adjacency_matrix[i][j] != 0) {
+                    if (cost[i] + adjacency_matrix[i][j] < cost[j]) {
                         cost[j] = cost[i] + adjacency_matrix[i][j];
                     }
                 }
@@ -39,19 +39,19 @@ int main(int argc, char **argv){
         }
 
         std::cout << "Iteration" << k << ":" << std::endl;
-        for(int i = 0; i < num_nodes; ++i){
-            std::cout << "node" << i+1 << " = " << cost[i] << ", ";
+        for (int i = 0; i < num_nodes; ++i) {
+            std::cout << "node" << i + 1 << " = " << cost[i] << ", ";
         }
         std::cout << "\n";
     }
 
     std::cout << "\n";
-    std::cout << "========================================================" <<std::endl;
+    std::cout << "========================================================" << std::endl;
     std::cout << "Final" << std::endl;
-    for(int i = 0; i < num_nodes; ++i){
-        std::cout << "node" << i+1 << " = " << cost[i] << ", ";
+    for (int i = 0; i < num_nodes; ++i) {
+        std::cout << "node" << i + 1 << " = " << cost[i] << ", ";
     }
     std::cout << "\n";
-    
+
     return 0;
 }
